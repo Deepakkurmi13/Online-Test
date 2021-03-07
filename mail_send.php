6ecd0d149eacfea0098774cabf244f4fc22ddbe0
DEEPAK KURMI <deepakkurmi874@gmail.com>
2021-03-07 14:49:07 +0530

    upade in code

--- mail_send.php
+++ mail_send.php
@@ -3,7 +3,7 @@
 $to = "deepakkurmi874@gmail.com"; 
 $subject = " Learning Management System";
 $message  =  "successfully resistered";
-$headers  = "From : deepakkurmi934@gmail  \r\n";
+$headers  = "From : deepakkurmi2021@gmail.com  \r\n";
 $headers .= "MIME-version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n"; 
 
